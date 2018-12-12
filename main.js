b81244693abdd803e33e32a1f1457134ea69a383
JayBo7 <jvboinapalli@ucdavis.edu>
2018-12-12 14:29:57 -0800

    place X's and set an interval for prompts

--- main.js
+++ main.js
@@ -1,13 +1,27 @@
 var prompt = require('prompt');
 
+var turn = 0;
+var matrix = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']];
+
 prompt.start();
 
-prompt.get(['x', 'y'], function (err, result) {
-	console.log(result.x, result.y)
-	console.log(`
-			 1 2 3		
-		1	| | | |
-		2	| | | |
-		3	| | | |
-	`)
-})
+const input = () => {
+
+	prompt.get(['x', 'y'], function (err, result) {
+		placeIcon('X', result);
+		console.log(`
+				 1 2 3		
+			1	|${matrix[0][0]}|${matrix[0][1]}|${matrix[0][2]}|
+			2	|${matrix[1][0]}|${matrix[1][1]}|${matrix[1][2]}|
+			3	|${matrix[2][0]}|${matrix[2][1]}|${matrix[2][2]}|
+		`)
+	})
+}
+
+const placeIcon = (player, result) => {
+	matrix[result.x - 1][result.y - 1] = player;
+}
+
+input();
+
+setInterval(input, 30000);
