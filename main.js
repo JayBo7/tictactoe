9ab6359b1461b9b737e10ffbd275cb4590f9f4f1
JayBo7 <jvboinapalli@ucdavis.edu>
2018-12-12 14:37:06 -0800

    keep track of turns and change player

--- main.js
+++ main.js
@@ -7,14 +7,18 @@ prompt.start();
 
 const input = () => {
 
+	var player = turn % 2 === 0 ? 'X' : 'O';
+
 	prompt.get(['x', 'y'], function (err, result) {
-		placeIcon('X', result);
+		placeIcon(player, result);
 		console.log(`
 				 1 2 3		
 			1	|${matrix[0][0]}|${matrix[0][1]}|${matrix[0][2]}|
 			2	|${matrix[1][0]}|${matrix[1][1]}|${matrix[1][2]}|
 			3	|${matrix[2][0]}|${matrix[2][1]}|${matrix[2][2]}|
 		`)
+		
+		turn++;
 	})
 }
 
@@ -24,4 +28,4 @@ const placeIcon = (player, result) => {
 
 input();
 
-setInterval(input, 30000);
+setInterval(input, 9000);
