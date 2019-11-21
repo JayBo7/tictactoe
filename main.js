63a126ed1c96cfb84d26c0b5bbb8934b65cba48b
JayBo7 <18199918+JayBo7@users.noreply.github.com>
2019-11-21 13:46:42 -0800

    determine winner

--- main.js
+++ main.js
@@ -3,29 +3,81 @@ var prompt = require('prompt');
 var turn = 0;
 var matrix = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']];
 
+var promptSchema = {
+	properties: {
+		x: {
+			pattern: /^[1-3]{1}$/,
+			message: 'Pick a value from 1 - 3',
+			required: true
+		},
+		y: {
+			pattern: /^[1-3]{1}$/,
+			message: 'Pick a value from 1 - 3',
+			required: true
+		}
+	}
+}
+
 prompt.start();
 
 const input = () => {
 
 	var player = turn % 2 === 0 ? 'X' : 'O';
 
-	prompt.get(['x', 'y'], function (err, result) {
+	console.log(`The current player is ${player}`);
+
+	prompt.get(promptSchema, function (err, result) {
 		placeIcon(player, result);
 		console.log(`
-				 1 2 3		
-			1	|${matrix[0][0]}|${matrix[0][1]}|${matrix[0][2]}|
-			2	|${matrix[1][0]}|${matrix[1][1]}|${matrix[1][2]}|
-			3	|${matrix[2][0]}|${matrix[2][1]}|${matrix[2][2]}|
+			   1 2 3		
+			1 |${matrix[0][0]}|${matrix[0][1]}|${matrix[0][2]}|
+			2 |${matrix[1][0]}|${matrix[1][1]}|${matrix[1][2]}|
+			3 |${matrix[2][0]}|${matrix[2][1]}|${matrix[2][2]}|
 		`)
-		
-		turn++;
+
+		var winner = determineWinner(player);
+
+		if (winner) {
+			console.log(`${winner} wins`);
+			return;
+		}
+
+		if (turn > 8) {
+			return;
+		}
+
+		input();
 	})
 }
 
 const placeIcon = (player, result) => {
-	matrix[result.x - 1][result.y - 1] = player;
+	if (matrix[result.x - 1][result.y - 1] !== ' ') {
+		return;
+	} else {
+		matrix[result.x - 1][result.y - 1] = player;
+		turn++;
+	}
 }
 
-input();
+const determineWinner = (player) => {
+	const winConditions = [[[1,1],[1,2],[1,3]], [[2,1],[2,2],[2,3]], [[3,1],[3,2],[3,3]], [[1,1],[2,1],[3,1]], [[1,2],[2,2],[3,2]], [[1,3],[2,3],[3,3]], [[1,1],[2,2],[3,3]], [[3,1],[2,2],[3,1]]]
 
-setInterval(input, 9000);
+	for (var i = 0; i < winConditions.length; i++) {
+		var stringKey = '';
+		for (var j = 0; j < winConditions[i].length; j++) {
+			stringKey += matrix[winConditions[i][j][0] - 1][winConditions[i][j][1] - 1];
+			if (stringKey === player + player + player) {
+				return player;
+			}
+		}
+	}
+}
+
+console.log(`
+			   1 2 3		
+			1 |${matrix[0][0]}|${matrix[0][1]}|${matrix[0][2]}|
+			2 |${matrix[1][0]}|${matrix[1][1]}|${matrix[1][2]}|
+			3 |${matrix[2][0]}|${matrix[2][1]}|${matrix[2][2]}|
+		`)
+
+input();
